7f407d88d673c2f9f815397f93c3b8f016b32cd8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-05-10 09:53:34 +0200

    Update to preview8

--- ImplicitCanvasAnimations.Browser/Program.cs
+++ ImplicitCanvasAnimations.Browser/Program.cs
@@ -1,6 +1,7 @@
 using System.Runtime.Versioning;
+using System.Threading.Tasks;
 using Avalonia;
-using Avalonia.Web;
+using Avalonia.Browser;
 
 [assembly:SupportedOSPlatform("browser")]
 
@@ -8,8 +9,8 @@ namespace ImplicitCanvasAnimations.Browser;
 
 internal class Program
 {
-    private static void Main(string[] args) 
-        => BuildAvaloniaApp().SetupBrowserApp("out");
+    private static async Task Main(string[] args) 
+        => await BuildAvaloniaApp().StartBrowserAppAsync("out");
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
